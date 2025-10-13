0e9334ec65ea2ac4c52755510146bb61f4d5e3b4
apoorvtechh <apoorvtechh@gmail.com>
2025-10-13 15:15:41 +0530

    changed ec2

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -220,7 +220,7 @@ def main():
             mlflow.set_tag("task", "Sentiment Analysis")
             mlflow.set_tag("dataset", "Reddit Comments")
 
-        logger.debug("Evaluation completed successfully ✅")
+        logger.debug("Evaluation completed successfully ✅")# adeed
 
     except Exception as e:
         logger.error("Evaluation pipeline failed: %s", e)
